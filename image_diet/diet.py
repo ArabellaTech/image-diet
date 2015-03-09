95cf4cf38b4a6c6484309570fdfa5d206c6afacb
Jacek <jacek@ivolution.pl>
2015-03-09 16:48:56 -0400

    AA-785

--- image_diet/diet.py
+++ image_diet/diet.py
@@ -1,7 +1,7 @@
 import logging
 import time
 import datetime
-from os.path import isfile
+import os
 from subprocess import call, PIPE
 from imghdr import what as determinetype
 import image_diet.settings as settings
@@ -51,19 +51,34 @@ def backup_copy(path):
     if settings.DIET_IMAGE_BACKUPS:
         timestamp = time.time()
         timestamp = datetime.datetime.fromtimestamp(timestamp).strftime('-%Y-%m-%d-%H:%M:%S')
-        call("cp '%(file)s' '%(file)s'.'%(timestamp)s'" % {'file': path, 'timestamp': timestamp},
+        call("cp '%(file)s' '%(file)s''%(timestamp)s'" % {'file': path, 'timestamp': timestamp},
              shell=True, stdout=PIPE)
 
 
-def squeeze(path, backup=settings.DIET_IMAGE_BACKUPS):
+def get_flag_path(path):
+    filename = os.path.basename(path)
+    directory = os.path.split(path)[0]
+    flagged_file_name = '.%s.%s' % (filename, settings.DIET_COMPRESSED_FLAG)
+    flag_path = os.path.join(directory, flagged_file_name)
+    return flag_path
+
+
+def create_processed_file_flag(path):
+    flag_path = get_flag_path(path)
+    call("touch %s" % flag_path, shell=True)
+
+
+def squeeze(path, backup=settings.DIET_IMAGE_BACKUPS, flag_processed_file=settings.DIET_FLAG_PROCESSED_FILE,
+            new_only=settings.DIET_NEW_FILES_ONLY):
     '''Returns path of optimized image or None if something went wrong.'''
-    if not isfile(path):
+    if not os.path.exists(path):
         logger.error("'%s' does not point to a file." % path)
         return None
 
     filetype = determinetype(path)
 
-    backup_copy(path)
+    if backup:
+        backup_copy(path)
 
     squeeze_cmd = ""
     if filetype == "jpeg":
@@ -73,11 +88,31 @@ def squeeze(path, backup=settings.DIET_IMAGE_BACKUPS):
     elif filetype == "png":
         squeeze_cmd = squeeze_png()
 
+    if new_only:
+        should_process_file = False
+        flag_path = get_flag_path(path)
+        flag_path = get_flag_path(path)
+
+        if not os.path.exists(flag_path):
+            should_process_file = True
+        else:
+            file_mt = os.path.getmtime(path)
+            flag_mt = os.path.getmtime(flag_path)
+
+            if flag_mt < file_mt:
+                should_process_file = True
+
+        if not should_process_file:
+            squeeze_cmd = False
+
     if squeeze_cmd:
         try:
             retcode = call(squeeze_cmd % {'file': path},
                            shell=True, stdout=PIPE)
+            if flag_processed_file:
+                create_processed_file_flag(path)
         except:
+            raise
             logger.error('Squeezing failed with parameters:')
             logger.error(squeeze_cmd[filetype] % {'file': path})
             logger.exception()
