ab17649005ce87277a8af11994d3f1fc7de2d120
jacoor <jacek@ivolution.pl>
2015-03-12 15:57:05 -0400

    Update file_system_storage.py

--- image_diet/file_system_storage.py
+++ image_diet/file_system_storage.py
@@ -9,7 +9,7 @@ class ImageDietFileSystemStorage(StaticFilesStorage):
     def post_process(self, files, *args, **kwargs):
         results = []
         print 'test'
-        print settings.DIET_COMPRESS_STATIC_IMAGES:
+        print settings.DIET_COMPRESS_STATIC_IMAGES
         die
         if settings.DIET_COMPRESS_STATIC_IMAGES:
             if 'image_diet' not in main_settings.INSTALLED_APPS:
